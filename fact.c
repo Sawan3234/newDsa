72913fa04fbc922538b2dff56140d7ced52df84b
SawanStha06 <sawanshrewta@gmail.com>
2024-02-13 21:17:10 +0545

    factorial using recursion

--- fact.c
+++ fact.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
+int fact (int n)
+{
+    if(n==0)
+    return 1;
+    else{
+        return (n*fact(n-1));
+    }
+}
 int main()
 {
-    printf("hello");
-    printf("push");
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+   int factorial=fact(num);
+    printf("factorial of %d is %d\n",num,factorial);
+    return 0;
+
 }
