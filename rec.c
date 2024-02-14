8d5cf845c0779b51534b2f303bb8609e57ff6864
SawanStha06 <sawanshrewta@gmail.com>
2024-02-14 15:01:38 +0545

    oh

--- rec.c
+++ rec.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
-int gcd(int ,int);
+int gcd(int n1 ,int n2)
+{
+     if(n2!=0)
+    return (n2,n1%n2);
+    else 
+    return n1;
+}
+
 int main()
 {
     int n1,n2,g;
     printf("enter two numbers:");
     scanf("%d %d ",&n1 ,&n2);
-    g=gcd(n1,n2);
-    printf("gcd of %d & %d:%d",n1,n2,g);
+    printf("gcd of %d & %d:%d",n1,n2,gcd(n1,n2));
     return 0;
-}
- int gcd(int n1,int n2)
-{
-    if(n2!=0)
-    return (n2,n1%n2);
-    else 
-    return n1;
 }
