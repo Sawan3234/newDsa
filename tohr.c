f75b897b9e17b21286761824bbcb54940e650e55
SawanStha06 <sawanshrewta@gmail.com>
2024-02-14 17:54:31 +0545

    tower of hanoi

---
+++ tohr.c
@@ -0,0 +1,22 @@
+ #include<stdio.h>
+ int toh(int,char,char,char);
+ int main()
+ {
+    int n;
+    char a,b,c;
+    printf("enter number of disk:");
+    scanf("%d",&n);
+    printf("tower of hanoi of %d\n",n);
+    toh(n,a,b,c);
+    return 0;
+}
+int toh(int n,char beg,char aux,char end)
+{
+    if(n==1)
+    printf("Move disc %c to %c",beg,end);
+    else{
+        toh(n-1,beg,end,aux);
+        toh(1,beg,aux,end);
+        toh(n-1,aux,beg,end);
+    }
+}
