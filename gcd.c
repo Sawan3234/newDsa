3060159e802b735adea84ccc50a2f0af6c934e40
SawanStha06 <sawanshrewta@gmail.com>
2024-02-14 14:20:34 +0545

    **

--- gcd.c
+++ gcd.c
@@ -4,8 +4,8 @@ int main()
 {
     int n1,n2,i,gcd;
     printf("enter two numbers:");
-    scanf("%d %d",&n1,&n2);
-    for(i=0;i<=n1 && i<=n2;i++)
+    scanf("%d%d",&n1,&n2);
+    for(i=1;i<=n1 && i<=n2;i++)
     {
         if(n1%i==0 && n2%i==0)
         gcd=i;
