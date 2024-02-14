3179666a3ab83ebeb6ede6c20d2664137c9d4348
SawanStha06 <sawanshrewta@gmail.com>
2024-02-14 19:34:28 +0545

    update

--- rec.c
+++ rec.c
@@ -2,7 +2,7 @@
 int gcd(int n1 ,int n2)
 {
      if(n2!=0)
-    return (n2,n1%n2);
+    return gcd(n2,n1%n2);
     else 
     return n1;
 }
--- tohr.c
+++ tohr.c
@@ -7,13 +7,13 @@
     printf("enter number of disk:");
     scanf("%d",&n);
     printf("tower of hanoi of %d\n",n);
-    toh(n,a,b,c);
+    toh(n,'a','b','c');
     return 0;
 }
 int toh(int n,char beg,char aux,char end)
 {
     if(n==1)
-    printf("Move disc %c to %c",beg,end);
+    printf("Move disc %c to %c\n",beg,end);
     else{
         toh(n-1,beg,end,aux);
         toh(1,beg,aux,end);
